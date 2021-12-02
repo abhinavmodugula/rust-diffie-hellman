5718e8aefb4b338d203f17b11b850eb508d659da
Austin Kaminow <austinkaminow@austins-mbp.umd.edu>
2021-12-02 12:10:43 -0500

    prettier

--- rust-diffie-hellman/src/austinmain.rs
+++ rust-diffie-hellman/src/austinmain.rs
@@ -1,67 +1,85 @@
-use image::RgbaImage;
+use image::RgbImage;
 use image::ImageBuffer;
 use rand::Rng;
 use image::GenericImageView;
+use image::Pixel;
+use std::io::Write;
+use std::io::prelude::*;
+use std::fs::File;
 
-fn do_xor(g: &[u8], s: &[u8]) -> Vec<u8> {
-	let mut vec = Vec::new();
-	for i in 0..s.len() {
-		vec.push(s[i]^g[i%g.len()]);	
-	}
-	vec
-}
-
-fn new_test() {
+fn pick_random () {
     let mut rng = rand::thread_rng();
     let mut rand_vec = Vec::new();
     for i in 0..rng.gen_range(20000..50000) {
-	let new_base: u64 = rng.gen_range(0..2_u64.pow(63));
-	let new_base_vec = new_base.to_ne_bytes();
-	rand_vec.append(&mut new_base_vec.to_vec());
+        let new_base: u64 = rng.gen_range(0..2_u64.pow(63));
+        let new_base_vec = new_base.to_ne_bytes();
+        rand_vec.append(&mut new_base_vec.to_vec());
     }
-    println!("{:?}", rand_vec);
-    let img = image::open("f1.jpeg").unwrap();
-    let (w, h) = img.dimensions();
-    let mut out: RgbaImage = ImageBuffer::new(w, h);
-    let mut count = 0;
-    for pixel in img.pixels() {
-        let mut other_pixel = out.get_pixel_mut(pixel.0, pixel.1);
-        let orig_pixel = pixel.2.clone().0;
-	let mut new_vals: [u8;4] = [0, 0, 0, 0];
-	for i in 0..4 {
-	    new_vals[i] = orig_pixel[i]^rand_vec[count%rand_vec.len()];
-	}
-        println!("{}, {}", orig_pixel[3], new_vals[3]);
-     	count += 1;
-    	*other_pixel = image::Rgba(new_vals);
+    let mut f = File::create("output.vtk").expect("Unable to create file");
+    for i in 0..rand_vec.len()-1 {
+        write!(f, "{},", rand_vec[i]);
     }
+    write!(f, "{}", rand_vec[rand_vec.len()-1]);
+    reader();
 
-    println!("dimensions of output image: {:?}", out.dimensions());
-    out.save("encrypted_f1.jpeg").unwrap(); //save the encyrpted image for later
-
-
-    //DECRYPT TIME!!!!
-    let img = image::open("encrypted_f1.jpeg").unwrap();
-    let (w, h) = img.dimensions();
-    let mut out: RgbaImage = ImageBuffer::new(w, h);
-    let mut count = 0;
-    for pixel in img.pixels() {
-        let mut other_pixel = out.get_pixel_mut(pixel.0, pixel.1);
-        let orig_pixel = pixel.2.clone().0;
-        let mut new_vals: [u8;4] = [0, 0, 0, 0];
-        for i in 0..3 { 
-            new_vals[i] = orig_pixel[i]^rand_vec[count%rand_vec.len()];
-        }
-	new_vals[3] = 255;
-        println!("{}, {}", orig_pixel[3], new_vals[3]);
-        count += 1;
-        *other_pixel = image::Rgba(new_vals);
-    }
-    
-    println!("dimensions of output image: {:?}", out.dimensions());
-    out.save("decrypted_f1.jpeg").unwrap();
 }
 
-fn main() {
-    new_test();
+fn reader() -> Vec<u8> {
+  let mut file = std::fs::File::open("output.vtk").unwrap();
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).unwrap();
+  let mut v: Vec<u8> = Vec::new();
+  for s in contents.split(",") {
+    v.push(s.trim().parse::<u8>().unwrap());
+  }
+  v
+}
+
+fn encrypt(filename: &str) {
+
+    let rand_vec = reader();
+    let img = image::open(filename.to_string()).unwrap();
+    let (w, h) = img.dimensions();
+    let mut image: RgbImage = ImageBuffer::new(w, h);
+    let mut count = 0;
+    for x in 0..w {
+	for y in 0..h {
+	    let mut new_vals: [u8;3] = [0, 0, 0];
+	    for i in 0..3 {
+	     	new_vals[i] = img.get_pixel(x, y).to_rgb().0[i]^rand_vec[count%rand_vec.len()];
+	    }
+    	    image[(x,y)] = image::Rgb(new_vals);
+	    count += 1;
+    	}
+    }
+
+    image.save("encrypted_f1.jpeg").unwrap(); //save the encyrpted image for later
+}
+
+fn decrypt(filename: &str) {
+
+    let rand_vec = reader();
+    let img = image::open(filename.to_string()).unwrap();
+    let (w, h) = img.dimensions();
+    let mut image: RgbImage = ImageBuffer::new(w, h);
+    let mut count = 0;
+    for x in 0..w {
+        for y in 0..h {
+            let mut new_vals: [u8;3] = [0, 0, 0];
+            for i in 0..3 {
+                new_vals[i] = img.get_pixel(x, y).to_rgb().0[i]^rand_vec[count%rand_vec.len()];
+            }
+            image[(x,y)] = image::Rgb(new_vals);
+            count += 1;
+        }
+    }
+
+    image.save("decrypted_f1.jpeg").unwrap(); //save the encyrpted image for later
+
+}
+
+fn main() { 
+    pick_random();
+    encrypt("f1.jpeg");
+    decrypt("encrypted_f1.jpeg");
 }
